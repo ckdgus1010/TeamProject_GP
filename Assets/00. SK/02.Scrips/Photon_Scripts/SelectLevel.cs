b01aa929c26e0faba726d81a8663546b6815f269
HNCSoo <58102022+HNCSoo@users.noreply.github.com>
2020-09-03 17:40:11 +0900

    SelectLevel 스크립트 추가

---
+++ Assets/00. SK/02.Scrips/Photon_Scripts/SelectLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SelectLevel : MonoBehaviour
+{
+    public static SelectLevel instance;
+    public Levels mLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    public void OnClick()
+    {
+        
+        if (!PhotonNetwork.IsMasterClient) return;
+
+        WatingButtonMgr.instance.myPhotonView.RPC("RpcSendLevel", RpcTarget.AllBuffered, mLevel);
+
+    }
+}
