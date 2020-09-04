58b14ddd1d6c544cf7a02f7ed1af712e27300dd0
HNCSoo <58102022+HNCSoo@users.noreply.github.com>
2020-09-04 09:59:51 +0900

    프리펩폴더 정리

---
+++ Assets/02. Scripts/SK/GameMap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMap : MonoBehaviour
+{
+    public GameObject[] mapList;
+    public GameObject[] QuestionList;
+    private GameObject map;
+    private GameObject levelQuestion;
+    public Transform levelPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        map = Instantiate(mapList[WatingButtonMgr.instance.mapCount]);
+        map.transform.position = transform.position;
+
+
+        levelQuestion = Instantiate(QuestionList[(int)WatingButtonMgr.instance.curruntLevels]);
+        levelQuestion.transform.position = levelPos.position ; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
