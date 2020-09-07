ae94902664e06ddd3f9df0d62ff4510995db4c03
haeri-park <haeri0915@naver.com>
2020-09-08 02:35:01 +0900

    BGM 헛수고(Feat. 방금 모기 잡았다)
    헛헛헛..

---
+++ Assets/02. Scripts/HR/SoundManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioClip cameraSound;
+    public AudioClip click;
+    public AudioClip correct;
+    public AudioClip create;
+    public AudioClip delete;
+    public AudioClip fail; 
+    public AudioClip fanal;    
+    public AudioClip hint;    
+    public AudioClip reset;    
+    public AudioClip throwaway;    
+    public AudioClip bgm0;
+    public AudioClip bgm1;
+    public AudioClip bgm2;
+    public AudioClip bgm3;
+    public AudioClip bgm4;
+    public AudioClip bgm5;
+}
