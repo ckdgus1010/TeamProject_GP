ca484adc1ef9b494c3a8654f9fe98c4ec7d918aa
haeri-park <haeri0915@naver.com>
2020-09-07 18:09:14 +0900

    해리 Sound 스크립트 추가
    ㅈㄱㄴ

---
+++ Assets/02. Scripts/HR/Sound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    public GameObject bGM_ON;
+    public GameObject bGM_OFF;
+    public GameObject effectSound_ON;
+    public GameObject effectSound_OFF;
+
+    public AudioClip bGM;
+    public AudioClip effectSound;
+
+
+    // public void bGM_ON()
+    // {
+    //     bGM_ON.SetActive(false);
+    //     bGM_ON.SetActive(true);
+    //     AudioBehaviour.enabled=false;
+    // }
+
+    //     public void bGM_OFF()
+    // {
+    //     bGM_OFF.SetActive(true);
+    //     bGM_OFF.SetActive(false);
+    //     AudioBehaviour.enabled=ture;
+    // }
+}
