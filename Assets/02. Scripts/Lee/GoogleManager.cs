868f5657235b5cb1d0408ab3612a57fc01db4d18
on_off <ckdgus3735@gmail.com>
2020-10-12 15:22:22 +0900

    201012 Lee - 구글 로그인 구현

---
+++ Assets/02. Scripts/Lee/GoogleManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GooglePlayGames;
+
+public class GoogleManager : MonoBehaviour
+{
+    public Text logText;
+
+    void Start()
+    {
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+    }
+
+    public void Login()
+    {
+        Social.localUser.Authenticate((bool success) =>
+        {
+            if (success) logText.text = Social.localUser.id + "\n" + Social.localUser.userName;
+            else logText.text = "구글 로그인 실패";
+        });
+    }
+
+    public void Logout()
+    {
+        ((PlayGamesPlatform)Social.Active).SignOut();
+        logText.text = "구글 로그아웃";
+    }
+}
---
+++ Assets/02. Scripts/Lee/VideoPlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private VideoPlayer videoPlayer;
+    public float timer = 1.5f;
+
+    // 영상이 끝났는지 확인
+    private bool isFinished = false;
+
+    // 영상이 끝난 후 활성화 시킬 Canvas
+    [SerializeField] private GameObject loginCanvas;
+
+    // 영상이 끝난 후 비활성화 시킬 object
+    [SerializeField] private GameObject introVideoCanvas;
+
+    void Start()
+    {
+        isFinished = false;
+    }
+
+    void Update()
+    {
+        if (isFinished == false && videoPlayer.isPrepared && videoPlayer.isPlaying == false)
+        {
+            Debug.Log("VideoCtrl ::: 영상 끝, 로그인 화면으로 이동");
+            
+            isFinished = true;
+            Invoke("ShowObjects", timer);
+        }
+    }
+
+    void ShowObjects()
+    {
+        loginCanvas.SetActive(true);
+        introVideoCanvas.SetActive(true);
+    }
+}
