f96c2852680b131041fe9bfdb22c69325e6cdcc6
MuMuKim <67031535+MuMuKim@users.noreply.github.com>
2020-10-15 18:15:08 +0900

    20201015 버튼사운드 04씬까지함

---
+++ Assets/02. Scripts/Ji/Scripts/ButtonSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSound : MonoBehaviour
+{
+    public AudioClip audioClrip;
+
+    private AudioSource buttonSound;
+    void Start()
+    {
+        buttonSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    public void ButtonClicked()
+    {
+        buttonSound.clip = audioClrip;
+        buttonSound.Play();
+    }
+}
